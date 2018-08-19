884d25b6ca9b65db9ec0329e80be6f0731c9de99
guillermopis <guillermo.pisqui@gmail.com>
2018-08-19 12:31:04 -0600

    update repo

---
+++ software_sky/database/db_structure.sql
@@ -0,0 +1,29 @@
+go
+-- first step
+if db_id('SkyrangerDB') is not null begin
+   print 'db exists'
+   alter database SkyrangerDB set single_user with rollback immediate
+   drop database SkyrangerDB;
+end
+
+--second step
+create database SkyrangerDB;
+go
+
+--third step
+use SkyrangerDB;
+go
+
+--four step, table user
+create table usuarios(
+	id int primary key not null identity,
+	usuario varchar(50),
+	contaseña varchar(100)
+);
+go
+
+
+
+
+
+
