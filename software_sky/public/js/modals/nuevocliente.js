a679ee813b60f52fddfb67bf549a99290670f21d
guillermopis <guillermo.pisqui@gmail.com>
2018-09-15 09:06:02 -0600

    valid

--- software_sky/public/js/modals/nuevocliente.js
+++ software_sky/public/js/modals/nuevocliente.js
@@ -163,21 +163,17 @@ var ModuloListado = function(){
 	}//fin de limpiar
 
 	_private.validarCampos=function() {
-		 'use strict';
-	  /*window.addEventListener('load', function() {*/
-	    // Fetch all the forms we want to apply custom Bootstrap validation styles to
 	    var forms = document.getElementsByClassName('needs-validation');
-	    // Loop over them and prevent submission
 	    var validation = Array.prototype.filter.call(forms, function(form) {
-	      /*form.addEventListener('submit', function(event) {*/
 	        if (form.checkValidity() === false) {
+						alert("el formulario es invalido");
 	          event.preventDefault();
 	          event.stopPropagation();
-	        }
+	        }else{
+						alert("el formulario es valido");
+					}
 	        form.classList.add('was-validated');
-	     /* }, false);*/
 	    });
-	 /* }, false);*/
 }// fin de funcion validar campos
 
 	_private.validarFormulario=function(){
@@ -233,7 +229,7 @@ var ModuloListado = function(){
 		}else{
 			botonGuardar[0].addEventListener('click', function(event){
 			//	_private.validarFormulario();
-				//_private.validarCampos();
+				_private.validarCampos();
 			});//fin de evento
 		}
 	}// fin de funcion evento a boton guardar
