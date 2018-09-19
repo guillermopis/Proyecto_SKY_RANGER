37749f0084af3d51b013e3d6b11b8ecf2cbca9c7
guillermopis <guillermo.pisqui@gmail.com>
2018-09-18 21:27:30 -0600

    modifique archivo insertar_Clientes.sql

--- software_sky/database/insertar_Clientes.sql
+++ software_sky/database/insertar_Clientes.sql
@@ -20,5 +20,5 @@ insert into tipo_moras(descripcion, porcentaje) values('Pago atrasado', 5);
 insert into clientes (nombre, nit,direccion_fiscal,direccion_facturacion,correo,telefono,
 estado,tipo_pago,tipo_servicio, fecha_ingreso,tipo_mora,saldo_Q, anticipo)values(
 'Guillermo Pisqui','333232-4','Avenida circunvalacion, Reu','calzada las palmas','guillermo.pisqui@gmail.com',
-'3456-9876', 'ACTIVO',1,1,'2018-08-12',1,23,6
+'3456-9876', 'ACTIVO',1,1,'2018/08/12',1,23,6
 );
