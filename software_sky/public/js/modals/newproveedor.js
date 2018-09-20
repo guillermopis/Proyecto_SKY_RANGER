1d239f0b94405a8d694a3f044b324ca7b7689383
ramirez <ramirez>
2018-09-20 08:52:20 -0600

    validar campos pro

--- software_sky/public/js/modals/newproveedor.js
+++ software_sky/public/js/modals/newproveedor.js
@@ -1,6 +1,7 @@
 var ModuloListado = function(){
 	var _private = {}, _public = {};
 	_private.formulario=null;
+
 	_public.__construct = function() {
 		return _public;
 	};
@@ -11,19 +12,22 @@ var ModuloListado = function(){
 		_private.agregarEventoAbotonGuardar();
 		_private.agregarEventoACheck1();
 		_private.agregarEventoAbotonCerrar();
-		/*_private.agregarEventoAbuscarNombre();
-		_private.agregarEventoAanterior();
+		//para paginacion
+		//_private.asignarEventoAbuscarPornombre();
+		/*_private.asignarEventoAbuscarPornombre();
 		_private.agregarEventoASiguiente();
+		_private.configuracionDePaginacion();
 		_private.traerTotal();*/
+		//_private.agregarEventoAanterior();
 	}
 
 	_private.agregarEventoAbotonCerrar=function(){
-		document.getElementById("bandera").style.display="none"
-		document.getElementById("id").style.display="none"
+		document.getElementById("bandera").style.display="none";
+		document.getElementById("id").style.display="none";
 		btncerrar = $("#btnCerrar");
 		console.log("boton cerrar si existe");
 		if(btncerrar.length == 0){
-			console.log("el boton cerrar de cliente no existe")
+			console.log("el boton cerrar de cliente no existe");
 		}else{
 			btncerrar[0].addEventListener('click', function(event){
 				_private.limpiar();
@@ -32,158 +36,22 @@ var ModuloListado = function(){
 		}//fin del if
 	}//fin de la funcion btncerrar
 
-
-		//aca esta la configuracion inicial de la paginacion
-	_private.traerTotal=function(){
-			$.ajax({
-						url: 'http://127.0.0.1:3000/vehiculos/{"a":"0", "b":"0","texto":"","placa":""}',
-						type: "GET"
-					}).done(function(data){
-						total = data.data.length;
-						if(total <= 5){
-							document.getElementById("anterior").style.display="none";
-							document.getElementById("siguiente").style.display="none";
-							document.getElementById("pagina").style.display="none";
-
-						}else{
-							document.getElementById("siguiente").style.display="block";
-							document.getElementById("pagina").style.display="block";
-						}
-					})//fin de ajax
-	}//fin de funcion traerTotal
-
-	_private.agregarEventoASiguiente=function(){
-			var siguiente = $("#siguiente");
-			if(siguiente.length==0){
-				console.log("el boton siguiente no existe");	return;
-			}else{
-				siguiente[0].addEventListener('click', function(event){
-					var n = document.getElementById("pagina").value;
-					var nu = (n*5);
-					//alert("estoy en siguiente total= "+total);
-					if(nu <= total){
-						if((nu+5) > total){ //predecimos si habra otra pagina
-							document.getElementById("siguiente").style.display="none";
-						}
-						document.getElementById("anterior").style.display="block";
-						$("#pagina").text(String(n+1));
-						document.getElementById("pagina").value=(n+1);
-						alert("estoy en siguiente n ="+	document.getElementById("pagina").value);
-						_private.hacerFiltro(nu,5);
-
-					}
-				});//fin de evento
-			}//fin de if
-	}// fin de funcion siguiente
-
-	_private.agregarEventoAanterior=function(){
-		document.getElementById("anterior").style.display="none";
-		var anterior = $("#anterior");
-		if(anterior.length==0){
-			console.log("el boton anterior no existe");
+	_private.agregarEventoAbotonGuardar = function(){
+		var botonGuardar = $("#btnguardarproveedor");
+		console.log('el btnguardar si existe');
+		if(botonGuardar.length==0){
+			console.log("el boton guardar no existe");
 			return;
 		}else{
-			anterior[0].addEventListener('click', function(event){
-			var n = document.getElementById("pagina").value;
-			//alert("estoy en anterior n= "+n);
-			if(n != 1){
-				if(n == 2){document.getElementById("anterior").style.display="none";}
-				document.getElementById("siguiente").style.display="block";
-				$("#pagina").text(String(n-1));
-				document.getElementById("pagina").value=(n-1);
-				var nu = ((n-2)*5);
-				_private.hacerFiltro(omitir,busque);
-			}else{
-				_private.hacerFiltro(omitir,busque);
-			}//fin de if = 0
+			botonGuardar[0].addEventListener('click', function(event){
+				_private.validarFormulario();
+
 			});//fin de evento
-		}//fin de if
-	}// fin de funcion anterior
-
-	_private.configuracionDePaginacion=function(total){
-		if(total<= 5){
-			document.getElementById("anterior").style.display="none";
-			document.getElementById("siguiente").style.display="none";
-			document.getElementById("pagina").value=(1);
-			$("#pagina").text("1");
-		}else{
-			document.getElementById("anterior").style.display="none";
-			document.getElementById("siguiente").style.display="block";
 		}
-	}// fin de funcion configuracionDePaginacion
+	}// fin de funcion evento a boton guardar
 
 
-	_private.asignarEventoAbuscarPorPlaca=function(){
-		var buscarP = $("#buscarPorPlaca");
-		if(buscarP.length==0){
-			console.log("el campo buscar por placa no existe");
-		}else{
-			buscarP[0].addEventListener('keyup',function(event){
-			//configuracion de la paginacion
-			_private.hacerFiltro(0);
-		});//fin de evento keyup
-		}//fin de if
-	}//fin de funcion buscar por placa
 
-	_private.asignarEventoAbuscarPorCliente=function(){
-		var buscarC = $("#buscarPorCliente");
-		if(buscarC.length==0){
-			console.log("el campo buscar por cliente no existe");
-		}else{
-			buscarC[0].addEventListener('keyup',function(event){
-				document.getElementById("anterior").style.display="none";
-				$("#pagina").text("1");
-			_private.hacerFiltro(omitir,busque);
-		});//fin de evento keyup
-		}//fin de if
-	}//fin de funcioi buscar por nombre de cliente
-
-	_private.hacerFiltro=function(omitir,buscque){
-		$.ajax({
-					url:  'http://localhost:3000/vehiculos/{"a":"'+omitir+'", "b":"'+busque+'","texto":"'+document.getElementById("buscarPorCliente").value+'","placa":"'+document.getElementById("buscarPorPlaca").value+'"}',
-					type: "GET"
-				}).done(function(data,message){ //cargamos a la tabla
-					//alert("AJAX ESTA RESPONDIENDO");
-					var total = data.data.length;
-					//alert("respondiendo");
-					if(total<=5){
-						document.getElementById("siguiente").style.display="none";
-					}else{
-						document.getElementById("siguiente").style.display="block";
-					}
-					//alert("estoy en hacerFiltro total= "+total);
-					$("#tablita").remove();
-					var b = '<tbody id="tablita" '+
-								"</tbody>";
-					$("#tablaVehiculos").append(b);
-					var respuestaTotal=data.data.length;
-					if(respuestaTotal>5){respuestaTotal=5}
-					for (var a = 0; a<respuestaTotal; a++){
-						//alert("respondiendo2");
-					var fila=
-					"<tr>"+
-						'<th scope="row"></th>'+
-						"<td>"+data.data[a].cliente.nombre+"</td>"+
-						"<td>"+data.data[a].marca+"</td>"+
-						"<td>"+data.data[a].linea+"</td>"+
-						"<td>"+data.data[a].modelo+"</td>"+
-						"<td>"+data.data[a].color+"</td>"+
-						"<td>"+data.data[a].placa+"</td>"+
-						"<td>"+data.data[a].estado+"</td>"+
-						"<td>"+
-						'<div class="input-group">'+
-						'<div class="input-group-append" id="btnver">'+
-							'<button type="button" class="buttonsmall hover"'+ 'onClick="ver('+data.data[a].id+')">'+
-							'<span class="fas fa-user-edit"></span>'+
-							"</button>"+
-						'</div>'+
-					'</div>'+
-						'</td>'+
-					"</tr>";
-						$("#tablita").append(fila);
-					}//fin del for
-				})//fin de ajax
-	}//fin de funcion hacer filtro
 
 		_private.agregarEventoACheck1=function(){
 				var check = $("#check1");
@@ -214,11 +82,11 @@ var ModuloListado = function(){
 	          event.preventDefault();
 	          event.stopPropagation();
 	        }else{
-						_private.validarFormulario(form.checkValidity());
+						__private.validarFormulario();
 					}
 	        form.classList.add('was-validated');
 	    });
-}// fin de funcion validar campos*/
+	}// fin de funcion validar campos*/
 
 
 
@@ -260,41 +128,14 @@ var ModuloListado = function(){
 						"correo_contacto": document.getElementById("correo_contacto").value						
 					}
 				}).done(function(data){
-					$('#modalnuevoproveedor').modal('hide')
-					alert("datos guardados");
+					$('#modalnuevoproveedor').modal('hide');
+					alert("Datos Guardados");
 					//volvemos a la pagina 
 					location.href = "http://localhost:8000/proveedores";
 				})//fin de ajax
 	}//fnin de funcino peticion
 
-	_private.agregarEventoAbotonGuardar = function(){
-		var botonGuardar = $("#btnguardarproveedor");
-		console.log('el btnguardar si existe');
-		if(botonGuardar.length==0){
-			console.log("el boton guardar no existe");
-			return;
-		}else{
-			botonGuardar[0].addEventListener('click', function(event){
-				_private.validarFormulario();
-
-			});//fin de evento
-		}
-	}// fin de funcion evento a boton guardar
-
 	
-	/*_private.agregarEventoAbotonGuardar = function(){
-		var btnGuardar = $("#btnguardarproveedor");
-		if(btnGuardar.length==0){
-			console.log("el boton guardar no existe");
-			return;
-		}else{
-			btnGuardar[0].addEventListener('click', function(event){
-			//	_private.validarFormulario();
-				_private.validarCampos();
-			});//fin de evento
-		}
-	}// fin de funcion evento a boton guardar*/
-
 	_private.asignarFormulario= function(){
 		var elementos = $("form");
 		if(elementos.length ==0){
@@ -304,18 +145,6 @@ var ModuloListado = function(){
 
 		}
 	}// fin de funcion  asignarFormulario
-	
-	/*_private.editarproveedor=function(){
-		var btneditar = $("btneditarproveedor");
-		if (btneditar.length==0){
-			console.log('el btneditarproveedor no existe');
-		}else{}
-			$("#btneditarproveedor")[0].addEventListener('click', function(event) {
-			//$('#modaleditarproveedor').modal('show');
-			$("#bandera").val("ver");
-			});//fin del evento click
-		}//fin del else
-	}//fin de la funcion editarproveedor*/
 
 	//este boton es para abrir el modal de nuevoprovedor
 	_private.agregarEventoAbotonNuevo=function(){
@@ -325,8 +154,8 @@ var ModuloListado = function(){
 			console.log('el btnnuevoproveedor no existe');
 		}else{
 			btnnuevo[0].addEventListener('click', function(event){
-			$('#modalnuevoproveedor').modal('show')
-			_private.limpiar()
+			$('#modalnuevoproveedor').modal('show');
+			_private.limpiar();
 			document.getElementById("btnguardarproveedor").disabled=false;
 			document.getElementById("check1").style.display="none";
 			document.getElementById("che").style.display="none";
