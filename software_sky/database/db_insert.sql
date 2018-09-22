b100f835fccfa68d22fa0ef9ffffbc4ba798534f
guillermopis <guillermo.pisqui@gmail.com>
2018-09-22 07:17:43 -0600

    avance de asignar gps a vehiculo

--- software_sky/database/db_insert.sql
+++ software_sky/database/db_insert.sql
@@ -2,3 +2,14 @@ insert into usuarios(usuario, contraseña) values('david','abc');
 insert into usuarios(usuario, contraseña) values('pancho','abc');
 insert into usuarios(usuario, contraseña) values('daniel','abc');
 insert into usuarios(usuario, contraseña) values('guillermo','abc');
+
+update usuarios set  rol=1 where id in(1,2,3,4)
+update usuarios set  estado='ACTIVO' where id in(1,2,3,4)
+
+update usuarios set  puesto='TECNICO' where id in(1,2)
+
+select * from usuarios
+
+select * from roles
+
+insert into roles(nombre)values('admin')
