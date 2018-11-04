2b1425b0618376f0b13c74a5730e9239a89bd0c9
guillermopis <guillermo.pisqui@gmail.com>
2018-11-04 11:51:35 -0600

    actualizando

---
+++ software_sky/public/js/modalvehiculo/funcionesVehiculo.js
@@ -0,0 +1,65 @@
+function verVehiculo(id_vehiculo){
+  $("#modalnuevovehiculo").modal("show");
+  $.ajax({
+      url: 'http://127.0.0.1:3000/vehiculos/{"a":"0", "b":"0","texto":"","placa":"","id":"'+id_vehiculo+'"}',
+      type: "GET",
+      dataType: 'json'
+    }).then(function(data) {
+      document.getElementById("dueñoV").value = data.data[0].cliente.nombre;
+      document.getElementById("marcave").value= data.data[0].marca;
+      document.getElementById("modelove").value = data.data[0].modelo;
+      document.getElementById("colorve").value= data.data[0].color;
+      document.getElementById("placave").value = data.data[0].placa;
+      document.getElementById("tipove").value= data.data[0].tipo;
+      document.getElementById("linea").value= data.data[0].linea;
+      document.getElementById("chasis").value= data.data[0].chasis;
+      document.getElementById("motor").value= data.data[0].motor;
+      document.getElementById("precioseve").value= data.data[0].precio_servicio;
+      document.getElementById("fechainstve").value= data.data[0].fecha_instalacion;
+      document.getElementById("estadove").value= data.data[0].estado;
+
+        $("#banderaV").val("verV");
+        $("#idV").val(id_vehiculo);
+        $('#formNuevoVehiculo').find('input, textarea, button, select').attr('disabled','disabled');
+        document.getElementById("check1V").checked=false;
+        document.getElementById("check1V").style.display="block";
+        document.getElementById("cheV").style.display="block";
+        document.getElementById("btnGuardarV").disabled=true;
+    });
+
+}//fin de funcion ver vehiculo
+
+function asignarGPS(id_vehiculo,placa){
+  //primero se comprueba que existan gps en stock
+  if(gpsentrada.length == 0){
+    alert("ERROR: no se puede asignar gps a vehiculo debido a que no hay GPS en stock, COMUNIQUESE CON EL ADMINISTRADOR");
+    $(modal).modal('hide');
+  }else{
+    listado.guardarV(id_vehiculo);
+    $("#modalasignargpsve").modal('show');
+    $("#placave2").val(placa);
+    document.getElementById("placave2").disabled=true;
+  //consultamos cual es el actual gps de este vehiculo.
+  $.ajax({
+    url:'http://127.0.0.1:3000/historialVehiculo/{"id":"'+id_vehiculo +'","a":"0", "b":"0","texto":""}',
+    type:"GET",
+    dataType: 'json',
+    data:{}
+  }).then(function(data){
+    if(data.data.length==0){
+      alert("Se le asignara GPS a este vehiculo por primera vez");
+      document.getElementById("idgpssalida").value='NULL';
+      document.getElementById("comentariove").value='Se asigno GPS a vehiculo por primera vez.'
+    }else{
+      listado.guardaridgpssalida(data.data[0].id_gps_entrada);
+      $.ajax({
+        url:'http://127.0.0.1:3000/gps/{"id":"'+data.data[0].id_gps_entrada+'","a":"0", "b":"5","estado":""}',
+        type:"GET",
+        data:{}
+      }).then(function(data){
+        document.getElementById("idgpssalida").value=data.data[0].imei;
+      })//fin de AJAX
+    }//fin de if
+  })//fin de funcion ajax
+  }//fin de if para comprobar gps en stock
+}//fin de funcion asignarGPS
---
+++ software_sky/public/js/modalvehiculo/modalAsignarGPS.js
@@ -0,0 +1,65 @@
+var ModuloListado = function(){
+  var idVehiculo2;
+  var idgpsSalida;
+	var _private = {};
+  var _public = {};
+	_public.__construct = function() {
+		return _public;
+	};
+  _public.guardaridgpssalida=function(gpsSalida){
+    idgpsSalida=gpsSalida;
+  }
+
+  _public.guardarV=function(idve){
+    idVehiculo2=idve
+  }
+
+	_public.iniciar=function(){
+		//_private.agregarEventoAcheckV();
+		_private.agregarEventoAbotonGuardarAsignacion();
+	}
+
+  _private.agregarEventoAbotonGuardarAsignacion=function(){
+    var btnGuardarAs = $("#btnGuardarAsignacion");
+    if(btnGuardarAs.lengh==0){
+      console.log("el boton guardar asignacion no existe");
+      return;
+    }else{
+      btnGuardarAs[0].addEventListener('click',function(event){
+        //var combo = document.getElementById("gpsentrada");
+        //var seleccionado = combo.options[combo.selectedIndex].text;
+        var forms = document.getElementsByClassName('needs-validation');
+        validarCampos(forms,event,function(estado){
+					_private.validarFormulario(estado);
+				})//fin de llamado a valida campos
+      });
+    }
+  }//fin de funcion a boton guardar
+
+  _private.validarFormulario=function(estado){
+    //alert("estoy en valdar formulario");
+    if(estado == true){
+      var gp = document.getElementById("idgpssalida").value;
+      if(gp=='NULL'){
+        idgpsSalida='NULL';
+      }
+        var url= "http://127.0.0.1:3000/historialVehiculo/"
+        var type= "POST"
+        var data={
+          "id_gps_entrada":document.getElementById("gpsentrada").value,
+          "id_gps_salida":idgpsSalida,
+          "idVehiculo":idVehiculo2,
+          "id_tecnico":document.getElementById("tecnico").value,
+          "comentario":document.getElementById("comentariove").value
+        };
+      redireccion="http://localhost:8000/vehiculo";
+      peticion(url,type,data,"modalasignargpsve",redireccion);
+    }else{
+
+    }
+  }//fin de funcion validar campos
+
+  return _public.__construct.apply(this, arguments);
+}
+var listado = new ModuloListado();
+document.addEventListener('DOMContentLoaded',listado.iniciar() , false);
